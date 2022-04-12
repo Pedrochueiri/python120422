f7dbaa0c5d3be3ae4119aaad6b49ac5878c1ecaf
Pedrochueiri <pedrochu99@hotmail.com>
2022-04-12 11:58:38 -0300

    a

---
+++ Exe5.py
@@ -0,0 +1,10 @@
+from string import whitespace
+
+
+x = int(input("Digite o numero natural x: "))
+y = int(input("Digite o numero natural y: "))
+
+res = x / y;
+while x - y:
+    print (res)
+    x -= y
